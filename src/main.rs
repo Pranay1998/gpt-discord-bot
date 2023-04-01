b7c23e9f805e35620dd6d57c0aaa5e5d2e5f21b3
Pranay Katta <pranay.katta@uber.com>
2023-04-01 00:05:48 -0400

    removing comments

--- src/main.rs
+++ src/main.rs
@@ -34,18 +34,9 @@ impl Handler {
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
             println!("{}", msg.content);
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 eprintln!("Error sending message: {:?}", why);
             }
@@ -119,12 +110,6 @@ impl EventHandler for Handler {
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -134,7 +119,6 @@ impl EventHandler for Handler {
 async fn main() -> Result<(), error::ServerError> {
     let discord_token = env::var("DISCORD_TOKEN")?;
     let openai_token = env::var("OPENAI_TOKEN")?;
-    // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT
@@ -142,16 +126,9 @@ async fn main() -> Result<(), error::ServerError> {
 
         let handler = Handler::new(openai_token);
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
     let mut client =
         SerenityClient::builder(discord_token, intents).event_handler(handler).await?;
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     client.start().await?;
     Ok(())
 }
