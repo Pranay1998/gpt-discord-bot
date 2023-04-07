5d84d2c9940693bf0be6d4c8d56b177ea0a49c52
Pranay Katta <pranay.katta@uber.com>
2023-04-07 17:54:36 -0400

    change print to println

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), lib::ServerError> {
-    print!("Server starting with pid {}...", std::process::id());
+    println!("Server starting with pid {}...", std::process::id());
     let discord_token = env::var("DISCORD_TOKEN")?;
     let openai_token = env::var("OPENAI_TOKEN")?;
 
